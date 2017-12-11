c09d0b1edeb01ef6585d626c68743a029ed766e8
Easy <240018840@qq.com>
2017-12-11 17:40:52 +0800

    去除json null

--- src/main/java/com/ec/common/model/BaseEntity.java
+++ src/main/java/com/ec/common/model/BaseEntity.java
@@ -7,12 +7,14 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 /**
  * 
  * @author winall
  *
  */
+@JsonSerialize(include=JsonSerialize.Inclusion.NON_NULL)
 public class BaseEntity {
 	
 	private   static final Logger   LOGGER = LoggerFactory.getLogger(BaseEntity.class);
--- src/test/java/com/ec/orders/Test.java
+++ src/test/java/com/ec/orders/Test.java
@@ -1,26 +1,11 @@
 package com.ec.orders;
 
+import com.ec.common.model.Response;
+
 public class Test {
 
 	public static void main(String[] args) {
-		/*Orders orders=new Orders();
-		orders.setId(IDGen.next());
-		orders.setDeliverAdderss("�㶫ʡ���������������ί�������");
-		orders.setCustomContent("��ע����㷢��");
-		orders.setCustomEmail("42000@qq.com");
-		orders.setCustomMobile("130458924477");
-		orders.setCustomName("�������");
-		orders.setCustomQq("232322323");
-		orders.setCustomWechat("whcat");
-		orders.setType("cccc");
-		orders.setProductBranch("ţ");
-		orders.setProductMaterial("����ʯ");
-		orders.setProductId("���ڷ�ۻ");
-		
-		
-		
-		System.err.println(orders);*/
-		System.err.println((System.currentTimeMillis()/1000/60/60)%24);
+		System.out.println(new Response<String>(Response.Code.SUCCESS.getValue()).toString());
 	}
 
 }
